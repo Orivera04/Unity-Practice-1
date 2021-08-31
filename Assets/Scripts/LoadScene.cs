506506b7a7683b5deaaea3d49e1a952d7c4c074e
Oscar Rivera <riveramoreira04@gmail.com>
2021-08-31 02:29:12 -0600

    Introduction Files.

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+	public string scene;
+
+	private void OnMouseDown()
+	{
+		SceneManager.LoadScene(scene);
+	}
+}
